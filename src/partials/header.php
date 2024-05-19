9442e3b508e7546ece0cbf21a3da4e63e95226c8
Nikola Stankovic <jonnywintech@gmail.com>
2024-05-19 19:25:02 +0200

    fxed most of css

--- src/partials/header.php
+++ src/partials/header.php
@@ -2,7 +2,7 @@
   <header class="header">
     <div class="wrap">
       <div class="header__container">
-        <a href="javascript:void(0)" class="header__logo">
+        <a href="index.php" class="header__logo">
           <img class="header__image" src="./src/images/header-logo.png" alt="company logo">
         </a>
         <nav class="header__nav">
@@ -32,6 +32,11 @@
             </li>
           </ul>
         </nav>
+        <button class="btn header__burger">
+          <svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2 s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2 S29.104,22,28,22z" />
+          </svg>
+        </button>
       </div>
     </div>
   </header>
